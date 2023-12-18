1d50158fb9db5a3f00df6f0265035f064ba5842a
Tarun-Zenitsu <btkkumbhar1998@gmail.com>
2023-12-18 23:56:30 +0530

    footer completed

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,8 +1,20 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className='border-t'>
+      <div className='flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row'>
+        <Link href='/'>
+          <Image 
+            src='/assets/images/logo.svg' alt='logo' width={120} height={38}
+          />
+        </Link>
+
+        <p>2023 EventPlaza. All Rights Reserved.</p>
+      </div>
+    </footer>
   )
 }
 

28cb1dd45b4b610bacd74b56e09093efad921383
Tarun-Zenitsu <btkkumbhar1998@gmail.com>
2023-12-27 23:35:05 +0530

    collection started

---
+++ components/shared/Collection.tsx
@@ -0,0 +1,41 @@
+import { IEvent } from '@/lib/database/models/event.model'
+import React from 'react'
+
+type CollectionProps = {
+    data: IEvent[],
+    emptyTitle: string,
+    emptyStateSubtext: string,
+    limit: number,
+    page: number | string,
+    totalPages?: number,
+    urlParamName?: string,
+    collectionType?: 'Events_Organized' | 'My_Tickets' | 'All_Events',
+    
+}
+
+const Collection = ({
+    data,
+    emptyTitle,
+    emptyStateSubtext,
+    page,
+    totalPages = 0,
+    collectionType,
+    urlParamName,
+}: CollectionProps) => {
+  return (
+    <>
+        {data.length > 0 ? (
+            <div>
+
+            </div>
+        ): (
+            <div>
+                <h3>{emptyTitle}</h3>
+                <h3>{emptyTitle}</h3>
+            </div>
+        )}
+    </>
+  )
+}
+
+export default Collection
